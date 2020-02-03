ed61e1872ef83e562ff774d59e8554267714846c
Xueying <xylu@utexas.edu>
2020-02-03 09:43:29 -0600

    Convergence tests scripts.

---
+++ convergence_test_space.sh
@@ -0,0 +1,5 @@
+./BiotSystem_EG 3 2.5e-4 0.0101 1e-7
+./BiotSystem_EG 4 2.5e-4 0.0101 1e-7
+./BiotSystem_EG 5 2.5e-4 0.0101 1e-7
+./BiotSystem_EG 6 2.5e-4 0.0101 1e-7
+./BiotSystem_EG 7 2.5e-4 0.0101 1e-7
---
+++ convergence_test_time.sh
@@ -0,0 +1,4 @@
+./BiotSystem_EG 7 0.25 1.0101 1e-7
+./BiotSystem_EG 7 0.2 1.0101 1e-7
+./BiotSystem_EG 7 0.1 1.0101 1e-7
+./BiotSystem_EG 7 0.05 1.0101 1e-7
