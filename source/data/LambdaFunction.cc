3628cb2b7855c92b727898c0b61126c3299f6934
Xueying <xylu@utexas.edu>
2020-02-23 17:26:49 -0600

    Updated heterogeneous test functions

--- source/data/LambdaFunction.cc
+++ source/data/LambdaFunction.cc
@@ -7,23 +7,23 @@ double LambdaFunction::value(const Point<dim> &p, const unsigned int component) 
     double nu = 0.2; // Poisson's ratio
     double x = p(0);
     double y = p(1);
-    if (x >= 0.1 && x <= 0.9 && y >= 0.5 - 1. / 64 && y <= 0.5 + 1. / 64)
+    
+    if (x >= 0.2 && x <= 0.8 && y >= 0.5 - 1. / 64 && y <= 0.5)
     {
         E = 1e4;
         nu = 0.05;
     }
-
-    if (x >= 0.1 && x <= 0.9 && y >= 0.25 - 1. / 64 && y <= 0.25 + 1. / 64)
+    if (x >= 0.2 && x <= 0.8 && y >= 0.25 - 1. / 64 && y <= 0.25)
     {
         E = 1e4;
         nu = 0.05;
     }
-    if (x >= 0.1 && x <= 0.9 && y >= 0.75 - 1. / 64 && y <= 0.75 + 1. / 64)
+    if (x >= 0.2 && x <= 0.8 && y >= 0.75 - 1. / 64 && y <= 0.75)
     {
         E = 1e4;
         nu = 0.05;
     }
-
+    
     double lame_lambda = E * nu / (1 + nu) / (1 - 2 * nu);
     return lame_lambda;
 }
--- source/data/PermFunction.cc
+++ source/data/PermFunction.cc
@@ -2,18 +2,26 @@
 using namespace std;
 double PermFunction::value(const Point<dim> &p, const unsigned int component) const
 {
-    double k =1e-4;
+    double k = 1e-4;
     double x = p(0);
     double y = p(1);
 
-    if (x >= 0.1 && x <=0.9 && y >= 0.5- 1./64 && y <= 0.5+ 1./64){
+    if (x >= 0.2 && x <= 0.8 && y >= 0.5 - 1. / 64 && y <= 0.5)
+    {
+        k = 10;
+    }
+    if (x >= 0.2 && x <= 0.8 && y >= 0.25 - 1. / 64 && y <= 0.25)
+    {
+        k = 10;
+    }
+    if (x >= 0.2 && x <= 0.8 && y >= 0.75 - 1. / 64 && y <= 0.75)
+    {
+        k = 10;
+    }
+/*
+    if (y<0.5){
         k=10;
     }
-        if (x >= 0.1 && x <=0.9 && y >= 0.25- 1./64 && y <= 0.25+ 1./64){
-        k=10;
-    }
-        if (x >= 0.1 && x <=0.9 && y >= 0.75- 1./64 && y <= 0.75+ 1./64){
-        k=10;
-    }
+    */
     return k;
 }
