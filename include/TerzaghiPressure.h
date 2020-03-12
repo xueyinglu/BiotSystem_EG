d74bce3305b309b23a8e0b767fcb62a604b5a1f8
Xueying <xylu@utexas.edu>
2020-03-12 13:43:04 -0500

    Corrected c_0 in the Terzaghi solution

--- include/TerzaghiPressure.h
+++ include/TerzaghiPressure.h
@@ -3,14 +3,14 @@
 #include "DealiiHeader.h"
 using namespace std;
 using namespace dealii;
-class TerzaghiPressure: public Function<dim>{
-    public:
-    TerzaghiPressure( double _t):
-    Function<dim> (1) {t = _t;};
+class TerzaghiPressure : public Function<dim>
+{
+public:
+    TerzaghiPressure(double _t) : Function<dim>(1) { t = _t; };
 
     virtual double value(const Point<dim> &p, const unsigned int component) const;
 
-    private:
+private:
     double t;
 };
 #endif
--- source/data/TerzaghiPressure.cc
+++ source/data/TerzaghiPressure.cc
@@ -3,16 +3,19 @@ using namespace std;
 
 double TerzaghiPressure::value(const Point<dim> &p, const unsigned int component) const
 {
+    // Follows Phillips thesis
     double k = 1e-3;
     double E = 1e5;
     double nu = 0.2;
     double alpha = 1;
-    double inv_M = 0.1;
+    // c_0 = inv_M;
+    double c_0 = 0.1;
     double lambda = E * nu / (1 + nu) / (1 - 2 * nu);
     double mu = E / 2 / (1 + nu);
     double K_bulk = lambda + 2. / 3 * mu;
-    double K_u = K_bulk + alpha * alpha * inv_M;
-    double c_f = inv_M * k * (K_bulk + 4. / 3 * mu) / (K_u + 4. / 3 * mu);
+    double K_u = K_bulk + alpha * alpha / c_0;
+    // fluid diffusivity coefficient
+    double c_f = 1./c_0 * k * (K_bulk + 4. / 3 * mu) / (K_u + 4. / 3 * mu);
 
     double F = 1e3; // traction
 
@@ -25,7 +28,7 @@ double TerzaghiPressure::value(const Point<dim> &p, const unsigned int component
     for (int i = 0; i < n_terms; i++)
     {
         double M = PI * (2 * i + 1) / 2;
-        pressure += alpha * F * inv_M / (K_u + 4. / 3 * mu) * 2 / M * sin(M * y) * exp(-M * M * c_f * t);
+        pressure += alpha * F /c_0 / (K_u + 4. / 3 * mu) * 2 / M * sin(M * y) * exp(-M * M * c_f * t);
         // pressure +=  2 / M * sin(M * y) * exp(-M * M * c_f * t);
     }
     if (t == 0)
