33b2d84d35223605077e44dab7751310f80a23dd
Xueying <xylu@utexas.edu>
2020-03-16 09:52:18 -0500

    Added Mandel grad p

--- include/MandelPressure.h
+++ include/MandelPressure.h
@@ -9,6 +9,7 @@ class MandelPressure : public Function<dim>
 public:
     MandelPressure(double _t) : Function<dim>(1) { t = _t; };
     virtual double value(const Point<dim> &p, const unsigned int component) const;
+    virtual void gradient_value(const Point<dim> & point, Tensor<1,dim> & gradient) const;
 
 private:
     double t;
