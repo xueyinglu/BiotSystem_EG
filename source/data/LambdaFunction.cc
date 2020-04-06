d12a0a491f4ec37fc55d1fd91a526145b7742ff3
Xueying <xylu@utexas.edu>
2020-04-06 18:47:24 -0500

    updated heterogeneous parameters

--- source/data/LambdaFunction.cc
+++ source/data/LambdaFunction.cc
@@ -7,7 +7,7 @@ double LambdaFunction::value(const Point<dim> &p, const unsigned int component) 
     double nu = 0.2; // Poisson's ratio
     double x = p(0);
     double y = p(1);
-    
+
     /* ARMA paper */ /*
     if (x >= 0.2 && x <= 0.8 && y >= 0.5 - 1. / 64 && y <= 0.5)
     {
@@ -36,27 +36,26 @@ double LambdaFunction::value(const Point<dim> &p, const unsigned int component) 
         nu = 0.05;
     }
 
-    if (x >= 21. / 64 && x <= 22. / 64 && y >= 12. / 64 && y <= 25. / 64)
+    if (x >= 21. / 64 && x <= 22. / 64 && y >= 16. / 64 && y <= 27. / 64)
     {
         E = 1e4;
         nu = 0.05;
     }
-    if (x >= 42. / 64 && x <= 43. / 64 && y >= 12. / 64 && y <= 25. / 64)
+    if (x >= 42. / 64 && x <= 43. / 64 && y >= 16. / 64 && y <= 27. / 64)
     {
         E = 1e4;
         nu = 0.05;
     }
-    if (x >= 21. / 64 && x <= 22. / 64 && y >= 39. / 64 && y <= 42. / 64)
+    if (x >= 21. / 64 && x <= 22. / 64 && y >= 37. / 64 && y <= 48. / 64)
     {
         E = 1e4;
         nu = 0.05;
     }
-    if (x >= 42. / 64 && x <= 43. / 64 && y >= 39. / 64 && y <= 42. / 64)
+    if (x >= 42. / 64 && x <= 43. / 64 && y >= 37. / 64 && y <= 48. / 64)
     {
         E = 1e4;
         nu = 0.05;
     }
-    
     double lame_lambda = E * nu / (1 + nu) / (1 - 2 * nu);
     return lame_lambda;
 }
