93ba4b53b193fd8aceb00087f66ed9ec8bf378fc
Xueying <xylu@utexas.edu>
2020-04-07 10:13:03 -0500

    corrected initial time default value

--- source/ParameterReader.cc
+++ source/ParameterReader.cc
@@ -21,7 +21,7 @@ void ParameterReader::declare_parameters()
         prm.declare_entry("Global refinement numbers", "3",
                           Patterns::Integer(0));
         
-        prm.declare_entry("Initial time", "1.0", Patterns::Double(0));
+        prm.declare_entry("Initial time", "0.0", Patterns::Double(0));
         
         prm.declare_entry("Final time", "1.0", Patterns::Double(0));
 
